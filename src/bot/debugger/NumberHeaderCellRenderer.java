74f3bee0ab29d33f5e53efce9f5f6493ac09de65
Tyler Smith <tyler.smith@oncospace.com>
2021-02-08 21:38:34 -0500

    update for problem that occurred after importing gradle into existing project. gradle build is now working.

--- src/bot/debugger/NumberHeaderCellRenderer.java
+++ src/bot/debugger/NumberHeaderCellRenderer.java
@@ -1,7 +1,5 @@
 package bot.debugger;
 
-import sun.swing.table.DefaultTableCellHeaderRenderer;
-
 public class NumberHeaderCellRenderer extends DefaultTableCellHeaderRenderer {
     public NumberHeaderCellRenderer() {
         setHorizontalAlignment(RIGHT);

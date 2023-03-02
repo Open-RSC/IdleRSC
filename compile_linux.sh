a0d3255b7491ae4ceed6af6571388b4583660ffa
George Rawlinson <george@rawlinson.net.nz>
2023-03-02 20:36:37 +1300

    build: soft deprecation of current build scripts
    Change current build scripts to run the gradle wrapper instead,
    so that the output jar is consistent.

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,14 +1,3 @@
-#!/bin/sh
-rm IdleRSC.jar
+#!/usr/bin/env bash
 
-find . | grep "\.java" > sources.txt
-
-javac -d bin -cp patched_client.jar @sources.txt
-
-jar cfe IdleRSC.jar bot.Main bin/*
-
-cd bin || exit
-jar -cvfm ../IdleRSC.jar ../META-INF/MANIFEST.MF ./*
-cd .. || exit
-
-rm sources.txt
+./gradlew build
--- compile_windows.bat
+++ compile_windows.bat
@@ -1,9 +1,1 @@
-del IdleRSC.jar
-dir /s /B *.java > sources.txt
-javac -d bin -cp patched_client.jar @sources.txt
-jar cfe IdleRSC.jar bot.Main bin/*
-cd bin/
-jar -cvfm ../IdleRSC.jar ../META-INF/MANIFEST.MF *
-cd ..
-del sources.txt
-pause
+gradlew.bat build

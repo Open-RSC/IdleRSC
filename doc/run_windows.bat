0441da73d5903ab05d4f862dc64f3486d40e3fa1
George Rawlinson <george@rawlinson.net.nz>
2023-04-05 20:30:05 +1200

    docs: add run_* scripts to doc/

---
+++ doc/run_windows.bat
@@ -0,0 +1,11 @@
+@echo off
+echo  [35m###############################################
+echo  #                                             #
+echo  #      [95mLaunching IdleRSC Botting Client[35m       #
+echo  #                                             #
+echo  ############################################### [0m
+
+java -jar IdleRSC.jar --username "username" --password "password"
+
+echo [91m CLIENT ERROR, CLOSING! [0m
+TIMEOUT /t 3

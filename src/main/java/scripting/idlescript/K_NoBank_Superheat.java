17781be0971b3f03b1caddba3c87e6ce4f5ef0a4
Kaila_Btw <javacube1@gmail.com>
2023-07-20 00:22:11 -0400

    New Scripts NoBankSuperheat and WhiteUnicorns
    NoBankSuperheat mines iron in khazard and uses superheat to make iron bars (100% completion chance) and then makes iron plates at the nearby anvil. Only resource required is nature runes. This method is designed for UIM smithing/magic training and is fairly efficient.
    
    WhiteUnicorns - kills the 2 white unicorns north west of catherby bank. This is extremely inefficient but meant as an alternative gathering method for hardcore accounts not wanting to visit the wilderness for unicorn horns.

---
+++ src/main/java/scripting/idlescript/K_NoBank_Superheat.java
@@ -0,0 +1,269 @@
+package scripting.idlescript;
+
+import java.awt.GridLayout;
+import java.util.Objects;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ * Mines Addy/Mith/Coal in Hobgoblin Mine and banks in Edge! (some pk/death protection).
+ *
+ * <p>
+ *
+ * <p>This bot supports the "autostart" parameter to automatiically start the bot without gui.
+ *
+ * <p>
+ *
+ * <p>Start in Varrock East bank or near Mine, with a pickaxe and bass key.
+ *
+ * <p>Sharks in bank REQUIRED.
+ *
+ * <p>@Author - Kaila
+ */
+public final class K_NoBank_Superheat extends K_kailaScript {
+  private static String isMining = "none";
+  private static int castsRemaining = 0;
+  private static int spellsCasted = 0;
+  private static final int IRON_ORE = 151, IRON_BAR = 170, SUPERHEAT_ID = 21, IRON_PLATE = 8;
+  private static final int[] currentOre = {0, 0};
+  private static final int[] ironIDs = {102, 103};
+
+  private boolean rockEmpty() {
+    if (currentOre[0] != 0) {
+      return c.getObjectAtCoord(currentOre[0], currentOre[1]) == 98;
+    } else {
+      return true;
+    }
+  }
+
+  private void startSequence() {
+    c.displayMessage("@red@No Bank Iron Superheat- By Kaila");
+    c.displayMessage("@red@Start in Khazard Mine with nats,hammer,fire staff,pickaxe");
+    if (c.isInBank()) {
+      c.closeBank();
+    }
+    if (!orsc.Config.C_BATCH_PROGRESS_BAR) c.toggleBatchBars();
+  }
+
+  public int start(String[] parameters) {
+    if (!guiSetup) {
+      setupGUI();
+      guiSetup = true;
+    }
+    if (scriptStarted) {
+      guiSetup = false;
+      scriptStarted = false;
+      startTime = System.currentTimeMillis();
+      startSequence();
+      scriptStart();
+    }
+    return 1000; // start() must return an int value now.
+  }
+
+  private void scriptStart() {
+    while (c.isRunning()) {
+      dropGems();
+      if (c.getInventoryItemCount(IRON_ORE) > 0) {
+        c.castSpellOnInventoryItem(
+            SUPERHEAT_ID,
+            c.getInventoryItemSlotIndex(IRON_ORE)); // c.getSpellIdFromName("Superheat Item") ]21]
+        c.sleep(2 * GAME_TICK);
+      }
+      if (c.getInventoryItemCount() < 30) {
+        if (rockEmpty() || !c.isBatching()) {
+          isMining = "none";
+          currentOre[0] = 0;
+          currentOre[1] = 0;
+        }
+      }
+      if (c.getInventoryItemCount() == 30) {
+        goToAnvil();
+        smithing();
+        anvilToOre();
+      }
+    }
+  }
+
+  private void smithing() {
+    // c.sleepHandler(98, true);
+    c.setStatus("@gre@Casting Superheat");
+    while (c.getInventoryItemCount(IRON_ORE) > 0) {
+      c.castSpellOnInventoryItem(
+          SUPERHEAT_ID,
+          c.getInventoryItemSlotIndex(IRON_ORE)); // c.getSpellIdFromName("Superheat Item") ]21]
+      c.sleep(2 * GAME_TICK);
+    }
+    castsRemaining = c.getInventoryItemCount(NATURE_RUNE);
+    totalBars = totalBars + c.getInventoryItemCount(IRON_BAR);
+    c.setStatus("@gre@Smithing");
+    if (c.getInventoryItemCount(IRON_BAR) > 0) {
+      c.useItemIdOnObject(568, 699, IRON_BAR);
+      c.sleep(2 * GAME_TICK);
+      c.optionAnswer(1);
+      c.sleep(GAME_TICK);
+      c.optionAnswer(2);
+      c.sleep(GAME_TICK);
+      c.optionAnswer(2);
+      c.sleep(GAME_TICK);
+      if (!c.isAuthentic()) {
+        c.optionAnswer(3);
+        c.sleep(3000); // was 650
+      }
+      waitForBatching();
+    }
+    totalPlates = totalPlates + c.getInventoryItemCount(IRON_PLATE);
+    if (c.getInventoryItemCount(IRON_BAR) > 0) { // make daggers if bars left over
+      c.useItemIdOnObject(568, 699, IRON_BAR);
+      c.sleep(2 * GAME_TICK);
+      c.optionAnswer(0);
+      c.sleep(GAME_TICK);
+      c.optionAnswer(0);
+      c.sleep(GAME_TICK);
+      c.optionAnswer(3);
+      c.sleep(GAME_TICK);
+      waitForBatching();
+    }
+    if (c.getInventoryItemCount(IRON_PLATE) > 0) {
+      c.dropItem(c.getInventoryItemSlotIndex(IRON_PLATE), c.getInventoryItemCount(IRON_PLATE));
+      c.sleep(GAME_TICK);
+      waitForBatching();
+    }
+  }
+
+  private void dropGems() {
+    dropItemAmount(UNC_SAPP, -1, true);
+    dropItemAmount(UNC_EMER, -1, true);
+    dropItemAmount(UNC_RUBY, -1, true);
+    dropItemAmount(UNC_DIA, -1, true);
+  }
+
+  private void mine(String i) {
+    if (Objects.equals(i, "iron")) {
+      int[] oreCoords = c.getNearestObjectByIds(ironIDs);
+      if (oreCoords != null) {
+        isMining = "iron";
+        c.atObject(oreCoords[0], oreCoords[1]);
+        currentOre[0] = oreCoords[0];
+        currentOre[1] = oreCoords[1];
+      }
+    }
+    c.sleep(1920);
+  }
+
+  private void goToAnvil() {
+    isMining = "none";
+    currentOre[0] = 0;
+    currentOre[1] = 0;
+    c.setStatus("@yel@Smelting..");
+    oreToAnvil();
+  }
+
+  private void anvilToOre() {
+    c.setStatus("@gre@Walking to Ore Rocks..");
+    c.walkTo(569, 697);
+    c.walkTo(573, 697);
+    c.walkTo(572, 706);
+    c.setStatus("@gre@Done Walking..");
+  }
+
+  private void oreToAnvil() {
+    c.setStatus("@gre@Walking to Anvil..");
+    c.walkTo(569, 697);
+    c.walkTo(569, 699);
+    totalTrips = totalTrips + 1;
+    c.setStatus("@gre@Done Walking..");
+  }
+  // GUI stuff below (icky)
+  private void setupGUI() {
+    JLabel header = new JLabel("NoBank Iron Superheat - By Kaila");
+    JLabel label1 = new JLabel("Start in Port Khazard Mine");
+    JLabel label2 = new JLabel("Required: Nats, hammer, fire staff, pickaxe");
+    JLabel label3 = new JLabel("This bot supports the \"autostart\" parameter");
+    JButton startScriptButton = new JButton("Start");
+
+    startScriptButton.addActionListener(
+        e -> {
+          scriptFrame.setVisible(false);
+          scriptFrame.dispose();
+          scriptStarted = true;
+        });
+
+    scriptFrame = new JFrame(c.getPlayerName() + " - options");
+
+    scriptFrame.setLayout(new GridLayout(0, 1));
+    scriptFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    scriptFrame.add(header);
+    scriptFrame.add(label1);
+    scriptFrame.add(label2);
+    scriptFrame.add(label3);
+    scriptFrame.add(startScriptButton);
+    scriptFrame.pack();
+    scriptFrame.setLocationRelativeTo(null);
+    scriptFrame.setVisible(true);
+    scriptFrame.requestFocusInWindow();
+  }
+
+  @Override
+  public void questMessageInterrupt(String message) {
+    if (message.contains("successfully")) spellsCasted++;
+  }
+
+  @Override
+  public void paintInterrupt() {
+    if (c != null) {
+      String runTime = c.msToString(System.currentTimeMillis() - startTime);
+      int plateSuccessPerHr = 0;
+      int barSuccessPerHr = 0;
+      int spellsPerHr = 0;
+      try {
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000L;
+        float timeRan = currentTimeInSeconds - startTimestamp;
+        float scale = (60 * 60) / timeRan;
+        plateSuccessPerHr = (int) (totalPlates * scale);
+        barSuccessPerHr = (int) (totalBars * scale);
+        spellsPerHr = (int) (spellsCasted * scale);
+      } catch (Exception e) {
+        // divide by zero
+      }
+      int x = 6;
+      int y = 15;
+      c.drawString("@red@NoBank Superheat@mag@~ by Kaila", x, 12, 0xFFFFFF, 1);
+      c.drawString("@whi@________________________", x, y, 0xFFFFFF, 1);
+      c.drawString("@whi@Bars Used: @gre@" + totalBars, x, y + 14, 0xFFFFFF, 1);
+      c.drawString(
+          "@whi@Bars Per Hr: @gre@" + String.format("%,d", barSuccessPerHr) + "@yel@/@whi@hr",
+          x,
+          y + (14 * 2),
+          0xFFFFFF,
+          1);
+      c.drawString("@whi@Platebodies Made: @gre@" + totalPlates, x, y + (14 * 3), 0xFFFFFF, 1);
+      c.drawString(
+          "@whi@Platebodies Per Hr: @gre@"
+              + String.format("%,d", plateSuccessPerHr)
+              + "@yel@/@whi@hr",
+          x,
+          y + (14 * 4),
+          0xFFFFFF,
+          1);
+      c.drawString(
+          "@whi@Spells Cast: @gre@"
+              + String.format("%,d", spellsCasted)
+              + " @yel@(@whi@"
+              + String.format("%,d", spellsPerHr)
+              + "@yel@/@whi@hr@yel@)",
+          x,
+          y + (14 * 5),
+          0xFFFFFF,
+          1);
+      c.drawString(
+          "@whi@Time Remaining: " + c.timeToCompletion(totalBars, castsRemaining, startTime),
+          x,
+          y + (14 * 6),
+          0xFFFFFF,
+          1);
+      c.drawString("@whi@Runtime: " + runTime, x, y + (14 * 7), 0xFFFFFF, 1);
+      c.drawString("@whi@__________________", x, y + 3 + (14 * 7), 0xFFFFFF, 1);
+    }
+  }
+}
---
+++ src/main/java/scripting/idlescript/K_WhiteUnicorns.java
@@ -0,0 +1,408 @@
+package scripting.idlescript;
+
+import java.awt.GridLayout;
+import javax.swing.*;
+import orsc.ORSCharacter;
+
+/**
+ * Edge Dungeon Hobs (and Skeleton/Zombie) - by Kaila
+ *
+ * <p>
+ *
+ * <p>Options: Combat Style, Loot level Herbs, Reg pots, Alter Prayer Boost, Food Type, and Food
+ * Withdraw Amount Selection, Chat Command Options, Full top-left GUI, regular atk/str pot option,
+ * and Autostart.
+ *
+ * <p>- cannot support bone looting with this bot due to the shape of the dungeon
+ *
+ * <p>@Author - Kaila
+ */
+public final class K_WhiteUnicorns extends K_kailaScript {
+  private static int totalUni = 0, inventUni = 0, uniInBank = 0;
+  private static final int[] lowLevelLoot = {
+    466, // unicorn horn
+    473
+  };
+
+  private static boolean isWithinLootzone(int x, int y) {
+    return c.distance(451, 470, x, y) <= 30; // center of lootzone
+  }
+
+  public int start(String[] parameters) {
+    if (parameters[0].toLowerCase().startsWith("auto")) {
+      foodId = 546;
+      fightMode = 0;
+      foodWithdrawAmount = 1;
+      potUp = false;
+      lootBones = true;
+      buryBones = true;
+      c.displayMessage("Got Autostart Parameter");
+      c.log("Auto-Starting using 1 Shark, controlled, Loot Low Level, no pot up", "cya");
+      c.log("Looting Bones, Banking bones", "cya");
+      guiSetup = true;
+      scriptStarted = true;
+    }
+    if (!guiSetup) {
+      setupGUI();
+      guiSetup = true;
+    }
+    if (scriptStarted) {
+      guiSetup = false;
+      scriptStarted = false;
+      startTime = System.currentTimeMillis();
+      c.displayMessage("@red@Edge Skeletons ~ Kaila");
+      c.displayMessage("@red@Start in Edge bank with Armor");
+
+      if (c.isInBank()) {
+        c.closeBank();
+      }
+      if (c.currentX() < 450) {
+        bank();
+        bankToHouse();
+        c.sleep(1380);
+      }
+      whatIsFoodName();
+      scriptStart();
+    }
+
+    return 1000; // start() must return an int value now.
+  }
+
+  private void scriptStart() {
+    while (c.isRunning()) {
+      int eatLvl = c.getBaseStat(c.getStatId("Hits")) - 20;
+      if (c.getCurrentStat(c.getStatId("Hits")) < eatLvl) {
+        eat();
+      }
+      checkFightMode();
+      if (potUp) {
+        attackBoost(0, false);
+        strengthBoost(0, false);
+      }
+      inventUni = c.getInventoryItemCount(466);
+      if (c.getInventoryItemCount() < 30 && c.getInventoryItemCount(foodId) > 0 && !timeToBank) {
+        if (!c.isInCombat()) {
+          looting();
+          if (buryBones) buryBones();
+          ORSCharacter npc = c.getNearestNpcById(0, true);
+          if (npc != null) {
+            c.setStatus("@yel@Attacking..");
+            c.attackNpc(npc.serverIndex);
+            c.sleep(2000);
+          } else {
+            looting();
+            if (lootBones) lootBones();
+            c.sleep(100);
+          }
+        } else {
+          c.sleep(640);
+        }
+      }
+      if (c.getInventoryItemCount() == 30
+          || c.getInventoryItemCount(foodId) == 0
+          || timeToBank
+          || timeToBankStay) {
+        c.setStatus("@yel@Banking..");
+        timeToBank = false;
+        houseToBank();
+        bank();
+        if (timeToBankStay) {
+          timeToBankStay = false;
+          c.displayMessage(
+              "@red@Click on Start Button Again@or1@, to resume the script where it left off (preserving statistics)");
+          c.setStatus("@red@Stopping Script.");
+          c.setAutoLogin(false);
+          c.stop();
+        }
+        bankToHouse();
+        c.sleep(618);
+      } else {
+        c.sleep(100);
+      }
+    }
+  }
+
+  private void lootBones() {
+    final int boneId = 20;
+    int[] coords = c.getNearestItemById(boneId);
+    if (coords != null && !c.isInCombat() && isWithinLootzone(coords[0], coords[1])) {
+      c.setStatus("@yel@No NPCs, Picking bones");
+      c.walkToAsync(coords[0], coords[1], 0);
+      c.pickupItem(coords[0], coords[1], boneId, true, false);
+      c.sleep(640);
+      if (buryBones) buryBones();
+    } else {
+      if (buryBones) buryBones();
+      c.sleep(100);
+    }
+  }
+
+  private void looting() {
+    for (int lootId : lowLevelLoot) {
+      try {
+        int[] coords = c.getNearestItemById(lootId);
+        if (coords != null && isWithinLootzone(coords[0], coords[1])) {
+          c.setStatus("@yel@Looting..");
+          c.walkToAsync(coords[0], coords[1], 0);
+          c.pickupItem(coords[0], coords[1], lootId, true, false);
+          c.sleep(640);
+        }
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  private void bank() {
+    c.setStatus("@yel@Banking..");
+    c.openBank();
+    c.sleep(640);
+    if (!c.isInBank()) {
+      waitForBankOpen();
+    } else {
+      totalUni = totalUni + c.getInventoryItemCount(466);
+      uniInBank = c.getBankItemCount(466);
+      for (int itemId : c.getInventoryItemIds()) {
+        c.depositItem(itemId, c.getInventoryItemCount(itemId));
+      }
+      c.sleep(1240); // Important, leave in
+      if (potUp) {
+        withdrawAttack(1);
+        withdrawStrength(1);
+      }
+      withdrawFood(foodId, foodWithdrawAmount);
+      bankItemCheck(foodId, 5);
+      c.closeBank();
+      c.sleep(1000);
+    }
+  }
+
+  private void eat() {
+    leaveCombat();
+    c.setStatus("@red@Eating..");
+
+    boolean ate = false;
+
+    for (int id : c.getFoodIds()) {
+      if (c.getInventoryItemCount(id) > 0) {
+        c.itemCommand(id);
+        c.sleep(700);
+        ate = true;
+        break;
+      }
+    }
+    if (!ate) { // only activates if hp goes to -20 again THAT trip, will bank and get new shark
+      // usually
+      c.setStatus("@red@We've ran out of Food! Running Away!.");
+      houseToBank();
+      bank();
+      bankToHouse();
+      c.sleep(618);
+    }
+  }
+
+  private void bankToHouse() {
+    c.setStatus("@gre@Walking to Unicorns..");
+    c.walkTo(440, 497);
+    c.walkTo(444, 498);
+    c.walkTo(445, 487);
+    c.walkTo(445, 477);
+    c.setStatus("@gre@Done Walking..");
+  }
+
+  private void houseToBank() {
+    c.setStatus("@gre@Walking to Bank..");
+    c.walkTo(445, 477);
+    c.walkTo(445, 487);
+    c.walkTo(444, 498);
+    c.walkTo(440, 497);
+    totalTrips = totalTrips + 1;
+    c.setStatus("@gre@Done Walking..");
+  }
+  // GUI stuff below (icky)
+  private void setupGUI() {
+    JLabel header = new JLabel("White Unicorns ~ by Kaila");
+    JLabel warning = new JLabel("This is inefficient and meant for HCIM");
+    JLabel label1 = new JLabel("Start by Catherby bank or by Unicorns");
+    JLabel label2 = new JLabel("Chat commands can be used to direct the bot");
+    JLabel label3 = new JLabel("::bank ::potup ::lootbones ::burybones");
+    JLabel label4 = new JLabel("Styles ::attack :strength ::defense ::controlled");
+    JLabel label5 = new JLabel("Param Format: \"auto\"");
+    JCheckBox lootBonesCheckbox = new JCheckBox("Pickup Bones?", true);
+    JCheckBox buryBonesCheckbox = new JCheckBox("Bury Bones?", true);
+    JCheckBox potUpCheckbox = new JCheckBox("Use regular Atk/Str Pots?", false);
+    JLabel fightModeLabel = new JLabel("Fight Mode:");
+    JComboBox<String> fightModeField =
+        new JComboBox<>(new String[] {"Controlled", "Aggressive", "Accurate", "Defensive"});
+    JLabel foodLabel = new JLabel("Type of Food:");
+    JComboBox<String> foodField = new JComboBox<>(foodTypes);
+    JLabel foodWithdrawAmountLabel = new JLabel("Food Withdraw amount:");
+    JTextField foodWithdrawAmountField = new JTextField(String.valueOf(1));
+    fightModeField.setSelectedIndex(0); // sets default to controlled
+    foodField.setSelectedIndex(2); // sets default to sharks
+    JButton startScriptButton = new JButton("Start");
+
+    startScriptButton.addActionListener(
+        e -> {
+          if (!foodWithdrawAmountField.getText().equals(""))
+            foodWithdrawAmount = Integer.parseInt(foodWithdrawAmountField.getText());
+          lootBones = lootBonesCheckbox.isSelected();
+          buryBones = buryBonesCheckbox.isSelected();
+          foodId = foodIds[foodField.getSelectedIndex()];
+          fightMode = fightModeField.getSelectedIndex();
+          potUp = potUpCheckbox.isSelected();
+          scriptFrame.setVisible(false);
+          scriptFrame.dispose();
+          scriptStarted = true;
+        });
+
+    scriptFrame = new JFrame(c.getPlayerName() + " - options");
+
+    scriptFrame.setLayout(new GridLayout(0, 1));
+    scriptFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    scriptFrame.add(header);
+    scriptFrame.add(warning);
+    scriptFrame.add(label1);
+    scriptFrame.add(label2);
+    scriptFrame.add(label3);
+    scriptFrame.add(label4);
+    scriptFrame.add(label5);
+    scriptFrame.add(lootBonesCheckbox);
+    scriptFrame.add(buryBonesCheckbox);
+    scriptFrame.add(potUpCheckbox);
+    scriptFrame.add(fightModeLabel);
+    scriptFrame.add(fightModeField);
+    scriptFrame.add(foodLabel);
+    scriptFrame.add(foodField);
+    scriptFrame.add(foodWithdrawAmountLabel);
+    scriptFrame.add(foodWithdrawAmountField);
+    scriptFrame.add(startScriptButton);
+    scriptFrame.pack();
+    scriptFrame.setLocationRelativeTo(null);
+    scriptFrame.setVisible(true);
+    scriptFrame.requestFocusInWindow();
+  }
+
+  @Override
+  public void chatCommandInterrupt(String commandText) { // ::bank ::lowlevel :potup ::prayer
+    if (commandText.contains("bank")) {
+      c.displayMessage("@or1@Got @red@bank@or1@ command! Going to the Bank!");
+      timeToBank = true;
+      c.sleep(100);
+    } else if (commandText.contains("bankstay")) {
+      c.displayMessage("@or1@Got @red@bankstay@or1@ command! Going to the Bank and Staying!");
+      timeToBankStay = true;
+      c.sleep(100);
+    } else if (commandText.contains("lootbones")) {
+      if (!lootBones) {
+        c.displayMessage("@or1@Got toggle @red@lootbones@or1@, turning on bone looting!");
+        lootBones = true;
+      } else {
+        c.displayMessage("@or1@Got toggle @red@bones@or1@, turning off bone looting!");
+        lootBones = false;
+      }
+      c.sleep(100);
+    } else if (commandText.contains("burybones")) {
+      if (!buryBones) {
+        c.displayMessage("@or1@Got toggle @red@bones@or1@, turning on bone bury!");
+        buryBones = true;
+      } else {
+        c.displayMessage("@or1@Got toggle @red@buryBones@or1@, turning off bone bury!");
+        buryBones = false;
+      }
+      c.sleep(100);
+    } else if (commandText.contains("potup")) {
+      if (!potUp) {
+        c.displayMessage("@or1@Got toggle @red@potup@or1@, turning on regular atk/str pots!");
+        potUp = true;
+      } else {
+        c.displayMessage("@or1@Got toggle @red@potup@or1@, turning off regular atk/str pots!");
+        potUp = false;
+      }
+      c.sleep(100);
+    } else if (commandText.contains(
+        "attack")) { // field is "Controlled", "Aggressive", "Accurate", "Defensive"}
+      c.displayMessage("@red@Got Combat Style Command! - Attack Xp");
+      c.displayMessage("@red@Switching to \"Accurate\" combat style!");
+      fightMode = 2;
+      c.sleep(100);
+    } else if (commandText.contains("strength")) {
+      c.displayMessage("@red@Got Combat Style Command! - Strength Xp");
+      c.displayMessage("@red@Switching to \"Aggressive\" combat style!");
+      fightMode = 1;
+      c.sleep(100);
+    } else if (commandText.contains("defense")) {
+      c.displayMessage("@red@Got Combat Style Command! - Defense Xp");
+      c.displayMessage("@red@Switching to \"Defensive\" combat style!");
+      fightMode = 3;
+      c.sleep(100);
+    } else if (commandText.contains("controlled")) {
+      c.displayMessage("@red@Got Combat Style Command! - Controlled Xp");
+      c.displayMessage("@red@Switching to \"Controlled\" combat style!");
+      fightMode = 0;
+      c.sleep(100);
+    }
+  }
+
+  @Override
+  public void questMessageInterrupt(String message) {
+    if (message.contains("You eat the")) {
+      usedFood++;
+    }
+  }
+
+  @Override
+  public void serverMessageInterrupt(String message) {
+    if (message.contains("You dig a hole")) {
+      usedBones++;
+    }
+  }
+
+  @Override
+  public void paintInterrupt() {
+    if (c != null) {
+
+      String runTime = c.msToString(System.currentTimeMillis() - startTime);
+      int successPerHr = 0;
+      int TripSuccessPerHr = 0;
+      long timeInSeconds = System.currentTimeMillis() / 1000L;
+
+      try {
+        float timeRan = timeInSeconds - startTimestamp;
+        float scale = (60 * 60) / timeRan;
+        successPerHr = (int) ((totalUni + inventUni) * scale);
+        TripSuccessPerHr = (int) (totalTrips * scale);
+
+      } catch (Exception e) {
+        // divide by zero
+      }
+      int x = 6;
+      int y = 21;
+      c.drawString("@red@White Unicorns @mag@~ by Kaila", x, y - 3, 0xFFFFFF, 1);
+      c.drawString("@whi@____________________", x, y, 0xFFFFFF, 1);
+      c.drawString("@whi@Horns in Bank: @gre@" + uniInBank, x, y + 14, 0xFFFFFF, 1);
+      c.drawString(
+          "@whi@Horns Picked: @gre@"
+              + (totalUni + inventUni)
+              + "@yel@ (@whi@"
+              + String.format("%,d", successPerHr)
+              + "@yel@/@whi@hr@yel@)",
+          x,
+          y + (14 * 2),
+          0xFFFFFF,
+          1);
+      c.drawString(
+          "@whi@Total Trips: @gre@"
+              + totalTrips
+              + "@yel@ (@whi@"
+              + String.format("%,d", TripSuccessPerHr)
+              + "@yel@/@whi@hr@yel@)",
+          x,
+          y + (14 * 3),
+          0xFFFFFF,
+          1);
+      c.drawString("@whi@Runtime: " + runTime, x, y + (14 * 4), 0xFFFFFF, 1);
+      c.drawString("@whi@____________________", x, y + 3 + (14 * 4), 0xFFFFFF, 1);
+    }
+  }
+}

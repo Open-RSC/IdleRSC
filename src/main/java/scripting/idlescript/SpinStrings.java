92723dfff6331b2c92a0ba7f2557ef0151061808
Kaila_Btw <javacube1@gmail.com>
2023-12-06 12:19:21 -0500

    Rewrite spinStrings to fix crafting bug
    DRY programming methodology, remove unnecessary repetition.
    Fix access modifiers,
    Speed up movement and ladders,
    Improve overall program flow
    remove loops

--- src/main/java/scripting/idlescript/SpinStrings.java
+++ src/main/java/scripting/idlescript/SpinStrings.java
@@ -5,33 +5,35 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import bot.Main;
+import controller.Controller;
 import models.entities.ItemId;
 
 /**
  * SpinStrings by Searos
  *
- * @author Searos
+ * @author Searos and Kaila
  */
 public class SpinStrings extends IdleScript {
-  final JComboBox<String> item = new JComboBox<>(new String[] {"Flax", "Wool"});
-  final JComboBox<String> destination =
+  private final Controller c = Main.getController();
+  private final JComboBox<String> item = new JComboBox<>(new String[] {"Flax", "Wool"});
+  private final JComboBox<String> destination =
       new JComboBox<>(new String[] {"Falador", "Seers", "Crafting Guild"});
-  JFrame scriptFrame = null;
-  boolean guiSetup = false;
-  boolean scriptStarted = false;
-  final int[] bankerIds = {95, 224, 268, 485, 540, 617};
-  final int[] bankX = {289, 500, 346};
-  final int[] bankY = {571, 455, 608};
-  final int[] inputIds = {ItemId.FLAX.getId(), ItemId.WOOL.getId()};
-  final int[] outputIds = {ItemId.BOW_STRING.getId(), ItemId.BALL_OF_WOOL.getId()};
-  int input = -1;
-  int output = -1;
-  int totalString = 0;
-  int bankedString = 0;
-  boolean upstairs = false;
-  boolean started = false;
-  int bankSelX = -1;
-  int bankSelY = -1;
+  private JFrame scriptFrame = null;
+  private boolean guiSetup = false;
+  private boolean scriptStarted = false;
+  private final int[] bankerIds = {95, 224, 268, 485, 540, 617};
+  private final int[] bankX = {289, 500, 346};
+  private final int[] bankY = {571, 455, 608};
+  private final int[] inputIds = {ItemId.FLAX.getId(), ItemId.WOOL.getId()};
+  private final int[] outputIds = {ItemId.BOW_STRING.getId(), ItemId.BALL_OF_WOOL.getId()};
+  private int input = -1;
+  private int output = -1;
+  private int totalString = 0;
+  private int bankedString = 0;
+  private int bankSelX = -1;
+  private int bankSelY = -1;
   /**
    * This function is the entry point for the program. It takes an array of parameters and executes
    * script based on the values of the parameters. <br>
@@ -47,227 +49,115 @@ public class SpinStrings extends IdleScript {
     if (scriptStarted) {
       guiSetup = false;
       scriptStarted = false;
+      if (c.getNearestNpcByIds(bankerIds, false) != null) {
+        if (c.getInventoryItemCount(input) == 0) {
+          bankingLoop();
+        }
+        walkToSpot();
+      }
       scriptStart();
     }
-
     return 1000; // start() must return a int value now.
   }
 
-  public void startWalking(int x, int y) {
-    // shitty autowalk
-    int newX = x;
-    int newY = y;
-    while (controller.currentX() != x || controller.currentY() != y) {
-      if (controller.currentX() - x > 23) {
-        newX = controller.currentX() - 20;
+  private void scriptStart() {
+    while (c.isRunning()) {
+      if (c.getNearestObjectById(121) != null
+          && c.getInventoryItemCount(input) > 0) {
+        c.setStatus("@gre@Spinning Flax");
+        int[] spinningWheel = c.getNearestObjectById(121);
+        if(destination.getSelectedIndex() == 0) {
+          c.walkTo(spinningWheel[0] + 1, spinningWheel[1]);
+        } else c.walkTo(spinningWheel[0], spinningWheel[1] - 1);
+        c.useItemIdOnObject(spinningWheel[0], spinningWheel[1], input);
+        waitWhileBatching();
       }
-      if (controller.currentY() - y > 23) {
-        newY = controller.currentY() - 20;
-      }
-      if (controller.currentX() - x < -23) {
-        newX = controller.currentX() + 20;
-      }
-      if (controller.currentY() - y < -23) {
-        newY = controller.currentY() + 20;
-      }
-      if (Math.abs(controller.currentX() - x) <= 23) {
-        newX = x;
-      }
-      if (Math.abs(controller.currentY() - y) <= 23) {
-        newY = y;
-      }
-      if (!controller.isTileEmpty(newX, newY)) {
-        controller.walkToAsync(newX, newY, 2);
-        controller.sleep(640);
-      } else {
-        controller.walkToAsync(newX, newY, 0);
-        controller.sleep(640);
-      }
+      // Checks for these actions inside the methods
+      walkToBank();
+      bankingLoop();
+      walkToSpot();
     }
   }
 
-  public void scriptStart() {
-    while (controller.isRunning()) {
-      // if player is near spinning wheel, not batching, and not banking, and seer's village
-      // selected. spin material.
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestObjectById(121) != null
-            && controller.getInventoryItemCount(input) > 0
-            && destination.getSelectedIndex() == 1) { // seers
-          controller.setStatus("@red@Spinning Flax in falador");
-          // if not batching use material on wheel
-          while (!controller.isBatching() && controller.getInventoryItemCount(input) > 0) {
-            // sleep if you have high fatigue
-            controller.sleepHandler(98, true);
-            controller.useItemIdOnObject(
-                controller.getNearestObjectById(121)[0],
-                controller.getNearestObjectById(121)[1],
-                input);
-            controller.sleep(640);
-          }
-          // if batching do nothing
-          while (controller.isBatching()) {
-            controller.sleep(340);
-          }
-        } else break;
+  private void bankingLoop() {
+    if (c.getInventoryItemCount(input) == 0) {
+      c.setStatus("@gre@Out of Input items");
+      if (!c.isInBank()) {
+        c.setStatus("@gre@Opening Bank");
+        c.openBank();
+        K_kailaScript.waitForBankOpen();
       }
-      // if player is near spinning wheel, not batching, and not banking, and falador selected. spin
-      // flax.
-      for (int i = 1; i <= 200; i++) {
-        if (destination.getSelectedIndex() == 0 // falador
-            && controller.getNearestObjectById(121) != null
-            && controller.getInventoryItemCount(input) > 0) {
-          controller.setStatus("@red@Spinning Flax");
-          // if not batching use material on wheel
-          while (!controller.isBatching() && controller.getInventoryItemCount(input) > 0) {
-            // sleep if you have high fatigue
-            controller.sleepHandler(98, true);
-            controller.useItemIdOnObject(
-                controller.getNearestObjectById(121)[0],
-                controller.getNearestObjectById(121)[1],
-                input);
-            controller.sleep(640);
-          }
-          // if batching do nothing
-          while (controller.isBatching()) {
-            controller.sleep(340);
-          }
-        } else break;
-      }
-      // if player is near spinning wheel, not batching, and not banking, and Craft guild
-      // selected. spin material.
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestObjectById(121) != null
-            && controller.getInventoryItemCount(input) > 0
-            && destination.getSelectedIndex() == 2) { // craft guild
-          controller.setStatus("@red@Spinning Flax in craft guild");
-          // if not batching use material on wheel
-          while (!controller.isBatching() && controller.getInventoryItemCount(input) > 0) {
-            // sleep if you have high fatigue
-            controller.sleepHandler(98, true);
-            controller.useItemIdOnObject(
-                controller.getNearestObjectById(121)[0],
-                controller.getNearestObjectById(121)[1],
-                input);
-            controller.sleep(640);
-          }
-          // if batching do nothing
-          while (controller.isBatching()) {
-            controller.sleep(340);
-          }
-        } else break;
-      }
-      // if player is not near spinning wheel and has materials, walk to spinning wheel
-      // if player is in seer's village walk to ladder and go up it
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestObjectById(121) == null
-            && controller.getInventoryItemCount(input) > 0
-            && destination.getSelectedIndex() == 1) { // seers
-          controller.setStatus("@red@Going upstairs");
-          // if the ladder at 525,462 is unloaded walk closer to it
-          while (controller.isTileEmpty(525, 462)) {
-            startWalking(524, 463);
-          }
-          // if the ladder is loaded climb up
-          while (!controller.isTileEmpty(525, 462)) {
-            controller.atObject(525, 462);
-            controller.sleep(5 * 640);
-          }
-        } else break;
-      }
-      // if player is not near spinning wheel and has materials, walk to spinning wheel
-      // if player is in CRAFT GUILD walk to ladder and go up it
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestObjectById(121) == null
-            && controller.getInventoryItemCount(input) > 0
-            && destination.getSelectedIndex() == 2) { // craft guild
-          controller.setStatus("@red@Going upstairs");
-          // if the ladder is loaded climb up
-          controller.walkTo(349, 611);
-          controller.sleep(640);
-          controller.atObject(349, 612);
-          controller.sleep(5 * 640);
-        } else break;
-      }
-      // if player is in falador, walk to the spinning wheel
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getInventoryItemCount(input) > 0
-            && destination.getSelectedIndex() == 0 // falador id
-            && controller.getNearestObjectById(121) == null) {
-          startWalking(577, 295);
-        } else break;
-      }
-      // when player has no more materials left, walk to the bank
-      // go down ladder and walk to seer's village bank
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestObjectById(121) != null
-            && controller.getInventoryItemCount(input) == 0
-            && destination.getSelectedIndex() == 1) { // seers
-          controller.setStatus("@red@Going downstairs");
-          controller.atObject(525, 1406);
-          controller.sleep(5 * 640);
-        } else break;
-      }
-      // when player has no more materials left, walk to the bank
-      // go down ladder and walk to craft guild bank
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestObjectById(121) != null
-            && controller.getInventoryItemCount(input) == 0
-            && destination.getSelectedIndex() == 2) { // craft guild
-          controller.setStatus("@red@Going downstairs");
-          controller.atObject(349, 1556);
-          controller.sleep(5 * 640);
-        } else break;
-      }
-      // walk to seers bank
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestNpcByIds(bankerIds, false) == null
-            && destination.getSelectedIndex() == 1) { // seers
-          controller.setStatus("@red@No banker visible, walking closer...");
-          controller.walkTo(291, 573);
-        } else break;
-      }
-      // walk to falador bank
-      for (int i = 1; i <= 200; i++) {
-        if (controller.getNearestNpcByIds(bankerIds, false) == null
-            && controller.getInventoryItemCount(input) == 0
-            && destination.getSelectedIndex() == 0) {
-          controller.setStatus("@red@Walking to bank");
-          startWalking(bankSelX, bankSelY);
-        } else break;
-      }
-
-      // open bank
-      for (int i = 1; i <= 200; i++) {
-        if (!controller.isInBank()
-            && (controller.getNearestNpcByIds(bankerIds, false) != null
-                || controller.getObjectAtCoord(346, 609) == 942)
-            && controller.getInventoryItemCount(input) == 0) {
-          controller.setStatus("@red@Banking");
-          controller.openBank();
-        } else break;
-      }
-      // deposit anything that is not a sleeping bag
-      totalString = totalString + controller.getInventoryItemCount(output);
-      if (controller.getInventoryItemCount(input) == 0 && controller.isInBank()) {
-        for (int itemId : controller.getInventoryUniqueItemIds()) {
-          if (itemId != 0 && itemId != 1263) {
-            controller.depositItem(itemId, controller.getInventoryItemCount(itemId));
-            controller.sleep(640);
+      // deposit all
+      totalString = totalString + c.getInventoryItemCount(output);
+      if (c.getInventoryItemCount(input) == 0 && c.isInBank()) {
+        for (int itemId : c.getInventoryUniqueItemIds()) {
+          if (itemId != 1263) {
+            c.depositItem(itemId, c.getInventoryItemCount(itemId));
           }
         }
-        controller.sleep(3000);
-        // withdraw materials
-        bankedString = controller.getBankItemCount(output);
-        controller.withdrawItem(input, 30);
-        controller.sleep(800);
-        controller.closeBank();
-        controller.setStatus("@red@Finished Banking");
+        c.sleep(2000);
+        bankedString = c.getBankItemCount(output);
+        c.withdrawItem(input, 30);
+        c.closeBank();
+        c.sleep(640);
+        c.setStatus("@gre@Finished Banking");
       }
     }
   }
 
-  public void setupGUI() {
+  private void walkToSpot() {
+    // Handle seers
+    if (c.getInventoryItemCount(input) > 0
+        && c.getNearestObjectById(121) == null) {
+      c.setStatus("@gre@Walking to Spot");
+      if (destination.getSelectedIndex() == 0) { // fally
+        c.walkTo(297,576);
+      }
+      if (destination.getSelectedIndex() > 0) { // seers or craft guild
+        int[][] walkToCoords = {{524, 462}, {349, 611}};
+        int[][] ladderCoords = {{525, 462}, {349, 612}};
+        c.setStatus("@gre@Walking to Ladder");
+        c.walkTo(
+            walkToCoords[destination.getSelectedIndex() - 1][0],
+            walkToCoords[destination.getSelectedIndex() - 1][1]);
+        c.setStatus("@gre@Going up the ladder");
+        c.atObject(
+            ladderCoords[destination.getSelectedIndex() - 1][0],
+            ladderCoords[destination.getSelectedIndex() - 1][1]);
+        c.sleep(2 * 640);
+      }
+    }
+  }
+
+  private void walkToBank() {
+    if (c.getInventoryItemCount(input) == 0) {
+      c.setStatus("@gre@Walking to Bank");
+      if (destination.getSelectedIndex() > 0
+        && c.getNearestObjectById(121) != null) { //seers or craft
+        int[][] walkToCoords = {{524, 1406}, {349, 1555}};
+        int[][] ladderCoords = {{525, 1406},{349, 1556}};
+        c.setStatus("@gre@Walking to Ladder");
+        c.walkTo(
+          walkToCoords[destination.getSelectedIndex() - 1][0],
+          walkToCoords[destination.getSelectedIndex() - 1][1]);
+        c.setStatus("@gre@Going downstairs");
+        c.atObject(
+            ladderCoords[destination.getSelectedIndex()-1][0],
+            ladderCoords[destination.getSelectedIndex()-1][1]);
+        c.sleep(2 * 640);
+      }
+      c.walkTo(bankSelX, bankSelY);
+    }
+  }
+
+  private void waitWhileBatching() {
+    c.sleep(3000);
+    while (c.isBatching()) {
+      c.sleep(640);
+    }
+  }
+
+  private void setupGUI() {
     JLabel header = new JLabel("FlaxtoString");
     JButton startScriptButton = new JButton("Start");
 
@@ -276,12 +166,12 @@ public class SpinStrings extends IdleScript {
           scriptFrame.setVisible(false);
           scriptFrame.dispose();
           scriptStarted = true;
-          controller.displayMessage("@gre@" + '"' + "heh" + '"' + " - Searos");
+          c.displayMessage("@gre@" + '"' + "heh" + '"' + " - Searos and Kaila");
           bankSelX = bankX[destination.getSelectedIndex()];
           bankSelY = bankY[destination.getSelectedIndex()];
           input = inputIds[item.getSelectedIndex()];
           output = outputIds[item.getSelectedIndex()];
-          controller.displayMessage("@red@FlaxtoString started");
+          c.displayMessage("@red@FlaxtoString started");
         });
 
     scriptFrame = new JFrame("Script Options");
@@ -302,10 +192,10 @@ public class SpinStrings extends IdleScript {
   @Override
   public void paintInterrupt() {
     if (controller != null) {
-      controller.drawBoxAlpha(7, 7, 128, 21 + 14 + 14, 0xFF0000, 64);
-      controller.drawString("@red@Spin Strings @gre@by Searos", 10, 21, 0xFFFFFF, 1);
-      controller.drawString("@red@Strings Spun: @yel@" + totalString, 10, 35, 0xFFFFFF, 1);
-      controller.drawString("@red@String in Bank: @yel@" + bankedString, 10, 49, 0xFFFFFF, 1);
+      c.drawBoxAlpha(7, 7, 128, 21 + 14 + 14, 0xFF0000, 64);
+      c.drawString("@red@Spin Strings @gre@by Searos and Kaila", 10, 21, 0xFFFFFF, 1);
+      c.drawString("@red@Strings Spun: @yel@" + totalString, 10, 35, 0xFFFFFF, 1);
+      c.drawString("@red@String in Bank: @yel@" + bankedString, 10, 49, 0xFFFFFF, 1);
     }
   }
 }

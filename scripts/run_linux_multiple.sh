933d1e338f6a767a551fce463b51d763221f906a
Ellie <13331364-Kaila_Btw@users.noreply.gitlab.com>
2025-01-27 17:35:02 +0000

    Update Helper Script Files

---
+++ scripts/run_linux_multiple.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+gnome-terminal "bash -c './run_linux_no_console.sh; exec bash'"
+gnome-terminal "bash -c './run_linux_no_console.sh; exec bash'"
+gnome-terminal "bash -c './run_linux_no_console.sh; exec bash'"
+gnome-terminal "bash -c './run_linux_no_console.sh; exec bash'"
+gnome-terminal "bash -c './run_linux_no_console.sh; exec bash'"
+gnome-terminal "bash -c './run_linux_no_console.sh; exec bash'"
--- scripts/run_linux_no_console.sh
+++ scripts/run_linux_no_console.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
 screen -dmS IdleRSC java -jar ./IdleRSC.jar
